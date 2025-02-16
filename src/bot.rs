e00c3231358a0a288d6b7eddfb6781c7e5ecc5f1
EthiopianMiddleChild <amannegesse@gmail.com>
2025-02-16 22:57:37 +0300

    added chatbot functionality

--- src/bot.rs
+++ src/bot.rs
@@ -1,10 +1,66 @@
+use minreq;
 use serde_json::{json, Value};
-use crate::telegram::{Message, User, Chat};
+use std::env;
 
-pub fn get_response(message: Message) -> Value {
+use crate::telegram::Message;
+
+pub fn get_response(message: &Message) -> Value {
+    //get response from AI
+    let api_token = env::var("AI_API_TOKEN").unwrap();
+
+    let ai_response: Value = match minreq::post("https://openrouter.ai/api/v1/chat/completions")
+        .with_header("Authorization", format!("Bearer {}", api_token))
+        .with_body(
+            json!({
+                "model": "deepseek/deepseek-r1:free",
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": "wake up"
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "Greetings"
+                    },
+                    {
+                        "role": "user",
+                        "content": format!("{}", message.text)
+                    },
+                ],
+                "response_format": {
+                    "type": "json_schema",
+                    "json_schema": {
+                        "name": "Name of model used",
+                        "strict": true,
+                        "schema": {
+                            "messages": {
+                                "role": "String",
+                                "text": "The response of the assistant without the thought process"
+                            }
+                        },
+                    },
+                },
+                "temperature": 0.5,
+            })
+            .to_string(),
+        )
+        .send()
+    {
+        Ok(res) => serde_json::from_str(res.as_str().unwrap()).unwrap(),
+        Err(_) => json!({}),
+    };
+
+    //handle and format response
+    let ai_message = ai_response["choices"]
+        .as_array()
+        .unwrap()[0]
+        ["message"]
+        .get("content")
+        .unwrap();
+
     let response = json!({
         "chat_id": message.get_chat_id(),
-        "text": message.text,
+        "text": ai_message,
         "parse_mode": "Markdown"
     });
 
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use serde_json::Value;
 use tiny_http::{Request, Response};
 use telegram::{Chat, User, Message};
 use dotenv::dotenv;
-use std::{env, sync::Arc};
+use std::env;
 
 mod bot;
 mod telegram;
@@ -11,6 +11,7 @@ mod telegram;
 const PORT: i16 = 8080;
 
 fn main() {
+
     //extract api_key
     dotenv().ok();
 
@@ -37,17 +38,18 @@ fn main() {
                 .read_to_string(&mut message_buffer)
                 .unwrap();
 
-            //respond to incoming request
-            let response = Response::empty(200);
-            if let Err(e) = request.respond(response) {
-                eprintln!("{}", e)
-            }
-
             //convert message to Json Value
             let message_json: Value = match serde_json::from_str::<Value>(message_buffer.as_str()) {
                 Ok(msg_b) => msg_b["message"].clone(),
                 Err(_) => {
-                    eprintln!("Message could not be parsed");
+                    let response = Response::
+                        from_string("Bad Request, Only works with telegram api requests")
+                        .with_status_code(400);
+                    if let Err(e) = request.respond(response) {
+                        eprintln!("{}", e)
+                    }
+
+                    eprintln!("Bad Request");
                     break 'thread;
                 }
             };
@@ -60,20 +62,33 @@ fn main() {
             let message: Message = match Message::new(message_json, &user, &chat) {
                 Ok(msg) => msg,
                 Err(e) => {
+                    let response = Response::
+                        from_string(format!("{}", e))
+                        .with_status_code(422);
+                    if let Err(e) = request.respond(response) {
+                        eprintln!("{}", e)
+                    }
+
                     eprintln!{"{}", e};
                     break 'thread
                 }
             };
 
-            println! {"Bot has recieved a message:\n\"{}\"\n from the user:\n{}",
+            println! {"Bot has recieved a message:\n\"{}\"\nfrom the user:\n{}",
                 &message.text,
                 &message.get_user_first_name()
             };
+            
+            //respond to incoming request
+            let response = Response::empty(200);
+            if let Err(e) = request.respond(response) {
+                eprintln!("{}", e)
+            }
 
-            //Responde to the player
-            let bot_message = bot::get_response(message);
+            //Respond to the player
+            let bot_message = bot::get_response(&message);
 
-            let api_token:Arc<String> = Arc::new(env::var("API_TOKEN").unwrap());
+            let api_token = env::var("TELEGRAM_API_TOKEN").unwrap();
 
             match minreq::post(format!(
                 "http://api.telegram.org/bot{}/sendMessage",
@@ -82,17 +97,10 @@ fn main() {
                 .with_header("Content-Type", "application/json")
                 .with_body(bot_message.to_string())
                 .send(){
-                    Ok(response) => println!("Replied to {} in a {} chat with: {}",
+                    Ok(_) => println!("Replied to {} in a {} chat with: {}",
                         &user.first_name,
                         &chat.chat_type,
-                        match serde_json::from_str::<Value>(response.as_str().unwrap())
-                            .unwrap()
-                            .get("result")
-                            .and_then(|r| r.get("text"))
-                            .and_then(|text| text.as_str()) {
-                                Some(text) => text,
-                                None => "failed"
-                            }
+                        &bot_message["text"]
                     ),
                     Err(e) => eprintln!("Error sending Telegram message: {}", e),
             }
